dca3bd4dc4bcb07125fc25bee71482838d59c639
Michael Tuttle <tuttlem@gmail.com>
2015-10-03 21:06:26 +1000

    Tabs to spaces

--- pulseaudio/run.sh
+++ pulseaudio/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 docker run  -d \
-			--name skype \
-			-v /etc/localtime:/etc/localtime:ro \
-			--device /dev/snd \
-			--name pulseaudio \
-			-p 4713:4713 \
-			-v /var/run/dbus:/var/run/dbus \
-			-v /etc/machine-id:/etc/machine-id \
-			tuttlem/pulseaudio
+      --name skype \
+      -v /etc/localtime:/etc/localtime:ro \
+      --device /dev/snd \
+      --name pulseaudio \
+      -p 4713:4713 \
+      -v /var/run/dbus:/var/run/dbus \
+      -v /etc/machine-id:/etc/machine-id \
+      tuttlem/pulseaudio
