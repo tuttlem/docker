0a0c4eb0c856f2bf07ece55ea282d1624f1c08a1
Michael Tuttle <tuttlem@gmail.com>
2015-10-08 21:16:03 +1000

    Updated run script

--- chrome/run.sh
+++ chrome/run.sh
@@ -17,8 +17,17 @@ if [ $? -eq 1 ]; then
            -v /dev/shm:/dev/shm \
            --name chrome \
            tuttlem/chrome
+
+else
+
+  if [ "$CHROME_RUNNING" == "false" ]; then
+    docker start chrome
+  else
+    # run a new instance if it's already running (should just be a new window)
+    # note that the --user-data-dir switch must align with the config data that we have mounted
+    # in the run command
+    docker exec chrome sh -c "/usr/bin/google-chrome '$@' --user-data-dir=/data"
+  fi
+
 fi
 
-if [ "$CHROME_RUNNING" == "false" ]; then
-  docker start chrome
-fi
